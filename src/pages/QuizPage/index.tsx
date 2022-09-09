c01cdcb85e8e779a2ef4a9125beef8ca84d832db
Low-profile <maxandy@naver.com>
2022-09-09 16:17:46 +0900

    style: UI 작업

--- src/pages/QuizPage/index.tsx
+++ src/pages/QuizPage/index.tsx
@@ -1,7 +1,22 @@
+import Button from 'components/Button';
+import QuizItem from 'components/QuizItem';
+import styles from './quizPage.module.scss';
+
 const QuizPage = () => {
   return (
     <>
-      <div>퀴즈페이지</div>
+      <p className={styles.quizIndex}>1/10</p>
+      <p className={styles.quizTitle}>1. 다음 중 가장 질문에 가까운 것은?</p>
+      <div className={styles.quizTimeBar}>Timebar</div>
+      <div className={styles.quizItems}>
+        <QuizItem>1번 질문</QuizItem>
+        <QuizItem>2번 질문</QuizItem>
+        <QuizItem>3번 질문</QuizItem>
+        <QuizItem>4번 질문</QuizItem>
+      </div>
+      <div className={styles.quizButton}>
+        <Button size="small">다음</Button>
+      </div>
     </>
   );
 };
