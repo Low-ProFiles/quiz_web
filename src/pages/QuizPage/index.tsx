797b5b7f4b21ed9f0c0444c54b273213649ba36c
Low-profile <maxandy@naver.com>
2022-09-14 15:33:13 +0900

    style: make effect when select item

--- src/pages/QuizPage/index.tsx
+++ src/pages/QuizPage/index.tsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react';
 import { scoreHandler } from 'redux/store';
 import { useNavigate } from 'react-router-dom';
 import { decode } from 'html-entities';
-import { CircularProgress } from '@mui/material';
-import { Box } from '@mui/system';
 
 import Button from 'components/Button';
 import QuizItem from 'components/QuizItem';
@@ -14,6 +12,9 @@ import styles from './quizPage.module.scss';
 const QuizPage = () => {
   const [index, setIndex] = useState(0);
   const [example, setExample] = useState<any[]>([]);
+  const [disabled, setDisabled] = useState(true);
+  const [answer, setAnswer] = useState<string>('');
+  const [buttonText, setButtonText] = useState('제출');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -21,14 +22,28 @@ const QuizPage = () => {
 
   const submitAnswer = (e: any) => {
     const question = response[index];
-    if (e.target.textContent === question.correct_answer) {
-      dispatch(scoreHandler());
-    }
-    if (index + 1 < response.length) {
-      setIndex(index + 1);
+    if (e.target.innerHTML === '다음') {
+      if (e.target.textContent === question.correct_answer) {
+        dispatch(scoreHandler());
+      }
+      if (index + 1 < response.length) {
+        setIndex(index + 1);
+      } else {
+        navigate('/result');
+      }
     } else {
-      navigate('/result');
+      if (response[index].correct_answer === answer) {
+        console.log('정답');
+      } else {
+        console.log('오답');
+      }
+      setButtonText('다음');
     }
+  };
+
+  const checkAnswer = (e: any) => {
+    setDisabled(false);
+    setAnswer(e.target.outerText);
   };
 
   useEffect(() => {
@@ -40,6 +55,8 @@ const QuizPage = () => {
         0,
         question.correct_answer,
       );
+      setDisabled(true);
+      setButtonText('제출');
       setExample(answers);
     }
   }, [response, index]);
@@ -58,12 +75,14 @@ const QuizPage = () => {
           </p>
           <div className={styles.quizItems}>
             {example.map(item => (
-              <QuizItem key={item}>{item}</QuizItem>
+              <QuizItem key={item} onClick={checkAnswer}>
+                {decode(item)}
+              </QuizItem>
             ))}
           </div>
           <div className={styles.quizButton}>
-            <Button size="small" onClick={submitAnswer}>
-              다음
+            <Button size="small" onClick={submitAnswer} disabled={disabled}>
+              {buttonText}
             </Button>
           </div>
         </>
